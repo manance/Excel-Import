be8f363f2ebee712c6ee67c7ca1d7464e2cd6d10
Patriks Puzulis <puzulispatriks@gmail.com>
2026-05-21 19:16:58 +0300

    Import form frontend

---
+++ resources/views/users/import.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excel Import</title>
+</head>
+<body>
+    <div>
+        <h1>Import your excel sheet here!</h1>
+        <form action="/user/import" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <input type="file" name="file">
+                <button type="submit">Import</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersImportController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/import', [UsersImportController::class, 'show']);
+Route::post('/users/import', [UsersImportController::class, 'store']);
